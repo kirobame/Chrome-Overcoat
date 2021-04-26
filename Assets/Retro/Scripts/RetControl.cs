31cc397c42883e9df451bac5ef12147209d5e54f
Robert Biaulet <r.biaulet@rubika-edu.com>
2021-04-26 16:20:48 +0200

    Setup

---
+++ Assets/Retro/Scripts/RetControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chrome.Retro
+{
+    public class RetControl : MonoBehaviour
+    {
+        
+    }
+}
